8ffaa335ab26e3080bac2e9aaf4f5a71f688a6c0
Walker <zero.stevewalker@gmail.com>
2022-04-09 16:19:35 +0800

    update

--- run.py
+++ run.py
@@ -4,9 +4,9 @@ spilt_list = []
 filtered_string = ''
 origin_string = ''
 
-subscribe_path = '/Users/walker/.config/clash/火星聯盟-學習交流.yaml'
-template_path = '/Users/walker/.config/clash/火星联盟-模板.yaml'
-save_path = '/Users/walker/.config/clash/火星联盟.yaml'
+subscribe_path = '/Users/walker/.config/clash/火星联盟.yaml'
+template_path = '/Users/walker/.config/clash/template.yaml'
+save_path = '/Users/walker/.config/clash/hxlm.yaml'
 
 with open(subscribe_path, 'r') as f: 
     file_string = f.read()
@@ -23,17 +23,22 @@ for str in origin_list:
 origin_string = origin_string[:-2]
 
 for str in spilt_list: 
-    if not str.find('NF') == -1: 
+    if (not str.find('NF') == -1) or (not str.find('解锁') == -1): 
         filtered_string = filtered_string + str
 filtered_string = filtered_string[:-2]
 
-proxy_groups = "\nproxy-groups: \n    "
-proxy_groups = proxy_groups + "- { name: 代理节点, type: select, proxies: [Auto, Fallback, " + origin_string + "]}\n    "
-proxy_groups = proxy_groups + "- { name: 媒体节点, type: select, proxies: [Auto-Medias, " + filtered_string + "]}\n    "
-proxy_groups = proxy_groups + "- { name: ---------------------------------------------, type: select, proxies: [DIRECT]}\n    - { name: Apple, type: select, proxies: [代理节点, DIRECT]}\n    - { name: Medias, type: select, proxies: [媒体节点, 代理节点, DIRECT]}\n    - { name: China-Websites, type: select, proxies: [DIRECT, 代理节点]}\n    - { name: Final, type: select, proxies: [代理节点, DIRECT]}\n    - { name: --------------------------------------------, type: select, proxies: [DIRECT]}\n    "
-proxy_groups = proxy_groups + "- { name: Auto, type: url-test, proxies: [" + origin_string + "], url: 'https://www.google.com/', interval: 86400 }\n    "
-proxy_groups = proxy_groups + "- { name: Auto-Medias, type: url-test, proxies: [" + filtered_string + "], url: 'http://fast.com/', interval: 86400 }\n    "
-proxy_groups = proxy_groups + "- { name: Fallback, type: fallback, proxies: [" + origin_string + "], url: 'https://www.google.com/', interval: 7200 }\n"
+proxy_groups = "\nproxy-groups: \n"
+proxy_groups = proxy_groups + "    - { name: 代理节点, type: select, proxies: [Auto, Fallback, " + origin_string + "]}\n"
+proxy_groups = proxy_groups + "    - { name: 媒体节点, type: select, proxies: [Auto-Medias, " + filtered_string + "]}\n"
+proxy_groups = proxy_groups + "    - { name: ---------------------------------------------, type: select, proxies: [DIRECT]}\n"
+proxy_groups = proxy_groups + "    - { name: Apple, type: select, proxies: [代理节点, DIRECT]}\n"
+proxy_groups = proxy_groups + "    - { name: Medias, type: select, proxies: [媒体节点, 代理节点, DIRECT]}\n"
+proxy_groups = proxy_groups + "    - { name: China-Websites, type: select, proxies: [DIRECT, 代理节点]}\n"
+proxy_groups = proxy_groups + "    - { name: Final, type: select, proxies: [代理节点, DIRECT]}\n"
+proxy_groups = proxy_groups + "    - { name: --------------------------------------------, type: select, proxies: [DIRECT]}\n"
+proxy_groups = proxy_groups + "    - { name: Auto, type: url-test, proxies: [" + origin_string + "], url: 'http://www.gstatic.com/generate_204', interval: 86400 }\n"
+proxy_groups = proxy_groups + "    - { name: Auto-Medias, type: url-test, proxies: [" + filtered_string + "], url: 'http://fast.com/', interval: 86400 }\n"
+proxy_groups = proxy_groups + "    - { name: Fallback, type: fallback, proxies: [" + origin_string + "], url: 'http://www.gstatic.com/generate_204', interval: 7200 }\n"
 
 with open(template_path, 'r') as f: 
     file_string = f.read()
